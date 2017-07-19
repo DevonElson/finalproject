929b627ae8a8fb390b4c7c04bc331872f2d095d3
Devon Elson <delson@westinghousehs.org>
2017-07-19 18:52:07 +0000

    my comment

--- script.js
+++ script.js
@@ -11,7 +11,59 @@ $("#guac").click(function(){
 $("#frose").click(function(){
     $("#song").hide();
     $("#secondsong").show();
+    
+    
 });
  
+$("#map1").hide(); 
+$("#map2").hide();
+$("#map3").hide();
 
+
+$("#apollo").click(function(){
+    $("#map2").hide();
+    $("#map3").hide();
+    $("#map1").show();
+});
+
+   
+         $("#msg").click(function(){
+         $("#map1").hide();
+         $("#map3").hide();
+         $("#map2").show();
+    
+    
+});
+
+        $("#bowl").click(function(){
+            $("#map1").hide();
+            $("#map2").hide();
+            $("#map3").show();
+            
+        });
+        
+        $("#apolloimg").hide();
+        $("#msgimg").hide();
+        $("#bowlimg").hide();
+        
+        $("#apollo").click(function(){
+             $("#msgimg").hide();
+             $("#bowlimg").hide();
+             $("#apolloimg").show();
+            
+            
+        });
+        
+        $("#msg").click(function(){
+            $("#apolloimg").hide();
+            $("#bowlimg").hide();
+            $("#msgimg").show();
+            
+        });
+        
+        $("#bowl").click(function(){
+            $("#apolloimg").hide();
+            $("#msgimg").hide();
+            $("#bowlimg").show();
+        });
 });
